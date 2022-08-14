3b9a5d17937f00b763b4f8206a1b9fca1262b203
ahsnl7 <ahsnl.mi@gmail.com>
2022-08-14 11:30:44 +0200

    init deployement setup

--- src/__init__.py
+++ src/__init__.py
@@ -1,14 +1,15 @@
 from flask import Flask
+from flask.json import jsonify
+from src.constants.http_status_codes import HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR
 import os
 from src.dashfone_home_page import stats, db, cors
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
-
     if test_config is None:
         app.config.from_mapping(
             SECRET_KEY=os.environ.get("SECRET_KEY"),
-            SQLALCHEMY_DATABASE_URI="postgresql://postgres:BlackHeist197@localhost/devdashf1",
+            SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DATABASE_URI"),
             SQLALCHEMY_TRACK_MODIFICATIONS=os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS")
         )   
     else:
@@ -18,5 +19,13 @@ def create_app(test_config=None):
     db.init_app(app)
     cors.init_app(app)
     app.register_blueprint(stats)
+
+    @app.errorhandler(HTTP_404_NOT_FOUND)
+    def handle_404(e):
+        return jsonify({'error': 'Not found'}), HTTP_404_NOT_FOUND
+
+    @app.errorhandler(HTTP_500_INTERNAL_SERVER_ERROR)
+    def handle_500(e):
+        return jsonify({'error': 'Something went wrong, we are working on it'}), HTTP_500_INTERNAL_SERVER_ERROR
 
     return app
